a28bf0de021dd1e5d65a2be9915fff6c68b5237f
RichardTamarisk <3200226264@qq.com>
2024-11-27 22:31:23 +0800

    * Modefied task.hpp

--- code/video_fusion_player/src/task.hpp
+++ code/video_fusion_player/src/task.hpp
@@ -3,12 +3,6 @@
 
 #include "common.h"
 
-struct FusionOptions {
-public:
-    // Indicates whether to apply lens distortion correction before fusion
-    bool apply_correction;
-};
-
 class Task {
 public:
     Task(int nums) {
@@ -87,88 +81,19 @@ public:
         return true;
     }
     /**
-     * Corrects the lens distortion of an image represented by an AVFrame and crops it.
-     *
-     * This function takes an input AVFrame, applies lens distortion correction,
-     * and crops the resulting image to remove unwanted borders.
-     *
-     * @param frame_input The input AVFrame containing the image data to be corrected.
-     * @param frame_output The output AVFrame that will contain the corrected and cropped image.
-     * @return True if the correction and cropping process is successful, false otherwise.
-     */
-    bool correct_image(AVFrame *frame_input, AVFrame *frame_output) {
-        if (!frame_input || !frame_output) {
-            return false;
-        }
-
-        // 将 AVFrame 转换为 cv::Mat
-        cv::Mat img(frame_input->height, frame_input->width, CV_8UC3, frame_input->data[0]);
-
-        // 创建输出图像
-        cv::Mat drcimg(img.rows, img.cols, CV_8UC3);
-        cv::Point lenscenter(img.cols / 2, img.rows / 2);
-        cv::Point2f src_a, src_b, src_c, src_d;
-        double r, s;
-        cv::Point2f mCorrectPoint;
-        double distance_to_a_x, distance_to_a_y;
-
-        // 校正每个像素
-        for (int row = 0; row < img.rows; row++) {
-            for (int cols = 0; cols < img.cols; cols++) {
-                r = sqrt((row - lenscenter.y) * (row - lenscenter.y) + (cols - lenscenter.x) * (cols - lenscenter.x)) * 0.56;
-                s = 0.9998 - 4.2932 * pow(10, -4) * r + 3.4327 * pow(10, -6) * pow(r, 2) - 2.8526 * pow(10, -9) * pow(r, 3) + 9.8223 * pow(10, -13) * pow(r, 4);mCorrectPoint = cv::Point2f((cols - lenscenter.x) / s * 1.35 + lenscenter.x, (row - lenscenter.y) / s * 1.35 + lenscenter.y);
-
-                // 越界判断
-                if (mCorrectPoint.y < 0 || mCorrectPoint.y >= img.rows - 1 || 
-                    mCorrectPoint.x < 0 || mCorrectPoint.x >= img.cols - 1) {
-                    continue;
-                }
-
-                // 计算临近点的坐标
-                src_a = cv::Point2f(static_cast<int>(mCorrectPoint.x), static_cast<int>(mCorrectPoint.y));
-                src_b = cv::Point2f(src_a.x + 1, src_a.y);
-                src_c = cv::Point2f(src_a.x, src_a.y + 1);
-                src_d = cv::Point2f(src_a.x + 1, src_a.y + 1);
-
-                // 计算新的像素值
-                distance_to_a_x = mCorrectPoint.x - src_a.x;
-                distance_to_a_y = mCorrectPoint.y - src_a.y;
-
-                for (int channel = 0; channel < 3; channel++) {
-                    drcimg.at<cv::Vec3b>(row, cols)[channel] =
-                        img.at<cv::Vec3b>(src_a.y, src_a.x)[channel] * (1 - distance_to_a_x) * (1 - distance_to_a_y) +
-                        img.at<cv::Vec3b>(src_b.y, src_b.x)[channel] * distance_to_a_x * (1 - distance_to_a_y) +
-                        img.at<cv::Vec3b>(src_c.y, src_c.x)[channel] * distance_to_a_y * (1 - distance_to_a_x) +
-                        img.at<cv::Vec3b>(src_d.y, src_d.x)[channel] * distance_to_a_y * distance_to_a_x;
-                }
-            }
-        }
-
-        // 裁剪
-        int topCropHeight = drcimg.rows * 0.126;
-        int bottomCropHeight = drcimg.rows * 0.126;
-        int leftCropWidth = drcimg.cols * 0.083;
-        int rightCropWidth = drcimg.cols * 0.085;
-
-        cv::Rect roi(leftCropWidth, topCropHeight, drcimg.cols - leftCropWidth - rightCropWidth, drcimg.rows - topCropHeight - bottomCropHeight);
-        cv::Mat croppedImg = drcimg(roi);
-
-        // 将结果填充到输出的 AVFrame
-        av_image_fill_arrays(frame_output->data, frame_output->linesize, croppedImg.data, AV_PIX_FMT_RGB24, croppedImg.cols, croppedImg.rows, 1);
-
-        return true;
-    }
-    /**
      * Fuses two AVFrames into a single fused frame.
      *
      * This function takes two input AVFrames, frame1 and frame2, and fuses them into a single output frame, frame_fused.
      * The fusion process is performed by averaging the pixel values of the two input frames.
-     * The behavior of the fusion process can be controlled by the options parameter, which specifies whether to apply
-     * lens distortion correction before fusion.
      *
      * @param frame1 The first input AVFrame.
      * @param frame2 The second input AVFrame.
      * @param frame_fused The output AVFrame that will contain the fused image.
+     * @param is_correct The boolean value will determine whether to perform image correction
+     * @return True if the fusion process is successful, false otherwise.
+     */
+    // This function declare here just to show interface design
+    // bool image_fusion(AVFrame *frame1, AVFrame *frame2, AVFrame *frame_fused, bool is_correct) {}
 
     void run() {
         AVFrame* frame1 = av_frame_alloc();
